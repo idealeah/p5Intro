e343fb3a0127eab0b5147928cb1b84d91d15592a
idealeah <willl973@newschool.edu>
2019-10-01 14:48:18 -0400

    noise

---
+++ noiseSketch.js
@@ -0,0 +1,21 @@
+let dotColor;
+let dotRad = 50;
+
+function setup() {
+  createCanvas(640, 480);
+  dotColor = color(20, 190, 250);
+  noStroke();
+}
+
+function draw() {
+  fill(dotColor);
+  ellipse(mouseX, mouseY, dotRad, dotRad);
+
+  if (mouseIsPressed) {
+    let r = noise(frameRate() / 300, millis() / 300) * 200;
+    let g = noise(millis() / 300) * 200;
+    let b = 230;
+    dotColor = color(r, g, b);
+    dotRad = noise(millis() / 400) * 100;
+  }
+}
