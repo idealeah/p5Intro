b3e2590e49448e6e96c89f166497e6f8388af032
idealeah <willl973@newschool.edu>
2019-10-20 16:59:34 -0400

    data update

--- scripts/fruitObject.js
+++ scripts/fruitObject.js
@@ -1,9 +1,33 @@
+fruitData = {
+    "apples": [
+        1, 7, 3
+    ],
+    "pears": [
+        1, 7, 3
+    ],
+    "oranges": [
+        1, 7, 3
+    ],
+}
+
+fruitData.apples[0];
+
+
+//OR
+
 fruitData = [{
-        fruit: apples,
-        price: 3
+        fruit: 'apples',
+        price: 5
     },
     {
-        fruit: pears,
+        fruit: 'pears',
         price: 10
-    }
+    },
+    {
+        fruit: 'oranges',
+        price: 8
+    },
 ]
+
+
+fruitData[0].price;
