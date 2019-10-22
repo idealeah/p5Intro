7f53f613e52d52df5d3b0a0c0da36c02162ab019
idealeah <willl973@newschool.edu>
2019-10-22 19:58:56 -0400

    demo

--- scripts/pokemonSimple.js
+++ scripts/pokemonSimple.js
@@ -4,12 +4,14 @@ function Pokemon(
   type,
   length,
   battleStats,
+  position
 ) {
   //properties
   this.name = name;
   this.type = type;
   this.length = length;
   this.battleStats = battleStats;
+  this.position = position;
 
   //a method
   this.greeting = function () {
@@ -18,59 +20,23 @@ function Pokemon(
 }
 
 
-
-
-
-
-
-
-
 //now let's make the sketch itself
 
 let mode; //which pokemon we are creating
-let pikaPic;
-let charPic;
-let pikaColor;
-let charColor;
 
 //store all our pokemon objects
 let pokemonArray = [];
 
-function preload() {
-  //load our pokemon images
-  pikaPic = loadImage("assets/pikachuSmall.png"); // Load the image
-  charPic = loadImage("assets/charizardSmall.png"); // Load the image
-}
 
 function setup() {
   createCanvas(800, 500);
   background(222, 254, 255);
   noStroke();
 
-  //set up some colors
-  pikaColor = color(255, 255, 0);
-  charColor = color(255, 174, 0);
 }
 
 function draw() {
   background(222, 254, 255);
-  //iterate through the array of pokemon objects, drawing and updating each one
-  for (let i = 0; i < pokemonArray.length; i++) {
-    pokemonArray[i].update();
-    pokemonArray[i].draw();
-    //test for collisions
-    for (let j = 0; j < pokemonArray.length; j++) {
-      let positionData1 = pokemonArray[i].positionData;
-      let positionData2 = pokemonArray[j].positionData;
-      let distance = positionData1.position.dist(positionData2.position);
-      if (
-        distance <= positionData1.radius / 2 + positionData2.radius / 2 &&
-        distance !== 0
-      ) {
-        console.log("overlap");
-      }
-    }
-  }
 }
 
 function mousePressed() {
@@ -84,14 +50,11 @@ function mousePressed() {
       "electric",
       16,
       [1, 0, 1],
-      pikaColor,
-      pikaPic,
       position
     );
     //add our new pikachu to the array
     pokemonArray.push(pikachu);
-    //have our pokemon draw and greet us
-    pikachu.draw();
+    //have our pokemon greet us
     pikachu.greeting();
     //print it to the console so we can see it
     console.log(pikachu);
@@ -103,14 +66,11 @@ function mousePressed() {
       "fire",
       67,
       [1, -30, 3],
-      charColor,
-      charPic,
       position
     );
     //add our new charizard to the array
     pokemonArray.push(charizard);
-    //have our pokemon draw and greet us
-    charizard.draw();
+    //have our pokemon greet us
     charizard.greeting();
     //print it to the console so we can see it
     console.log(charizard);
